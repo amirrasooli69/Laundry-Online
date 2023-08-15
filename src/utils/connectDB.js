0e6bb251ab6795343a217a6fd10cd846430b4ace
amirrasooli69 <amirrasooli69@gmail.com>
2023-08-15 11:19:51 +0330

    add comment for debugigng

--- src/utils/connectDB.js
+++ src/utils/connectDB.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 async function connectDB(){
     if(mongoose.connections[0].readyState) return;
     mongoose.set("strictQuery", false);
+    console.log("////////////////////////////////////////////////////")
     console.log(process.env.MONGO_URI)
+    console.log(typeof(process.env.MONGO_URI))
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected To DB");
 }
