073e8b4a9226fc53ce35e4dfd697620e396d95e5
amirrasooli69 <amirrasooli69@gmail.com>
2023-08-15 14:07:01 +0330

    work on register on db

--- src/utils/connectDB.js
+++ src/utils/connectDB.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
 async function connectDB(){
+    const test="mongodb+srv://amirrasooli69:aammiirr69@cluster0.j1hazht.mongodb.net/?retryWrites=true&w=majority"
     if(mongoose.connections[0].readyState) return;
     mongoose.set("strictQuery", false);
     console.log("////////////////////////////////////////////////////")
-    console.log(process.env.MONGO_URI)
-    console.log(typeof(process.env.MONGO_URI))
-    await mongoose.connect(process.env.MONGO_URI);
+
+    // await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(test);
     console.log("Connected To DB");
 }
 
