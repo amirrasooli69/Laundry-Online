2cc3d9111ee32001e672c4a50d47e564f5f44d90
amirrasooli69 <amirrasooli69@gmail.com>
2023-10-17 13:43:36 +0330

    change in BuyResidential page

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -1,17 +1,17 @@
 import BuyResidentialPage from "@/template/BuyResidentialPage";
 
 async function BuyResidential({searchParams}) {
-  const res = await fetch("http://localhost:3000/api/profile", {
-    cache: "no-store",
-  });
-  const data = await res.json();
-  let finalData = data.data;
-  if(searchParams.category){
-    finalData = finalData.filter(i => i.category == searchParams.category);
-  }
+  // const res = await fetch("http://localhost:3000/api/profile", {
+  //   cache: "no-store",
+  // });
+  // const data = await res.json();
+  // let finalData = data.data;
+  // if(searchParams.category){
+  //   finalData = finalData.filter(i => i.category == searchParams.category);
+  // }
   return (
     <div>
-      <BuyResidentialPage  data={finalData}/>
+      {/* <BuyResidentialPage  data={finalData}/> */}
     </div>
   );
 }
